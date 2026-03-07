608c458b1ab4470867ce663d2bf9dfe0087ecfbd
Luciferstrike123 <130897548+Luciferstrike123@users.noreply.github.com>
2026-03-07 14:18:12 +0700

    Add swagger

--- backend/src/master-data/master-data.module.ts
+++ backend/src/master-data/master-data.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MasterDataService } from './master-data.service';
 import { MasterDataController } from './master-data.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [MasterDataService],
   controllers: [MasterDataController]
 })
