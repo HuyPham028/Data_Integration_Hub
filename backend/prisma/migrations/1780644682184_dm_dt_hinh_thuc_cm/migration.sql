550e2d9b6f1a2bd70668a793e7b0a8a23b13dab0
HuyPham028 <130897548+HuyPham028@users.noreply.github.com>
2026-06-05 14:31:24 +0700

    chore(schema): apply drift migration for "dm_dt_hinh_thuc_cm"

---
+++ backend/prisma/migrations/1780644682184_dm_dt_hinh_thuc_cm/migration.sql
@@ -0,0 +1,12 @@
+-- DropIndex
+DROP INDEX "dm_dt_hinh_thuc_cm_ma_key";
+
+-- DropIndex
+DROP INDEX "dm_dt_hinh_thuc_cm_ma_idx";
+
+-- AlterTable
+ALTER TABLE "dm_dt_hinh_thuc_cm" ADD COLUMN     "noiDaoTao" TEXT,
+ALTER COLUMN "ma" DROP NOT NULL;
+
+-- CreateIndex
+CREATE INDEX "dm_dt_hinh_thuc_cm_id_idx" ON "dm_dt_hinh_thuc_cm"("id");
