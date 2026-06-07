476898d2f9436cf77fef27d04fc4791eb94259f0
tvkspor <tmtuan120704@gmail.com>
2026-06-07 16:39:24 +0700

    Update Add LogModule

--- backend/src/modules/api-log/api-log.module.ts
+++ backend/src/modules/api-log/api-log.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ApiLogController } from './api-log.controller';
 import { ApiLogService } from './api-log.service';
 import { ApiLogInterceptor } from './api-log.interceptor';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ApiLogController],
   providers: [ApiLogService, ApiLogInterceptor],
   exports: [ApiLogInterceptor],
